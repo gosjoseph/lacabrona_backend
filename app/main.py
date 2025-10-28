9a5c0d6e5a4c00d41953caa0933f4722be911e17
Adrian Gomez <227512891+gosjoseph@users.noreply.github.com>
2025-10-27 22:52:11 -0300

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from integrations.MongoDB.router import router as mongo_router
 
 app = FastAPI(title="Lacabrona API", version="0.1.0")
@@ -6,6 +7,13 @@ app = FastAPI(title="Lacabrona API", version="0.1.0")
 
 app.include_router(mongo_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
